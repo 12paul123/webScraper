1e9a728210e85f09e13a365d7dfd3e77f1b31b2d
12paul123 <49698178+12paul123@users.noreply.github.com>
2020-10-25 12:05:00 +0100

    email

--- blocket.py
+++ blocket.py
@@ -1,11 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
-import time
+import time, os
 import asyncio
+import smtplib
+import ssl
 
-URL_1 = "https://www.blocket.se/annonser/hela_sverige/fordon/batar/segelbat?cg=1062"
-URL_2 = "https://www.blocket.se/annonser/hela_sverige/fritid_hobby/musikutrustning/gitarr_bas_forstarkare?cg=6161"
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+site = "https://www.blocket.se"
+URL_1 = site + "/annonser/hela_sverige/fordon/batar/segelbat?cg=1062"
+URL_2 = site + "/annonser/hela_sverige/fritid_hobby/musikutrustning/gitarr_bas_forstarkare?cg=6161"
+
 session = requests.Session()
+
+def init_server():
+	context = ssl.create_default_context()
+	server.ehlo()
+	server.starttls(context=context)
+	server.ehlo()
+	server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+async def send(ad, current_time):
+	section = "".join(ad[0]).encode("ascii", "ignore").decode("ascii")
+	subject = "Ad Found! {}, {}".format(current_time, section)
+	body = "Price: {} URL: {}{}".format(ad[1], site, ad[2])
+	msg = f"Subject: {subject}\n\n{body}"
+
+	try:
+		server.sendmail(EMAIL_ADDRESS, "opticdragonbf3@gmail.com", msg)
+	except Exception as e:
+		print(e)
+		server.close()
 
 async def parse_site(url):
 	page = session.get(url, verify=True)
@@ -17,7 +43,6 @@ async def get_latest_ad(url):
 	ad_elems = soup.find_all("article", class_="hidZFy")
 	latest_ad = ad_elems[0]
 
-	# hitta namn, pris och url
 	ad_name = latest_ad.find("span", class_ = "jzzuDW").text
 	ad_price = latest_ad.find("div", class_ = "jkvRCw").text
 	ad_url = latest_ad.find("a", class_="enigRj")["href"]
@@ -29,18 +54,22 @@ async def search_url(url):
 
 	while True:
 		ad = await get_latest_ad(url)
+
+		t = time.localtime()
+		current_time = time.strftime("%H:%M:%S", t)
+
 		if current_ad[0] == ad[0]:
 			await asyncio.sleep(300)
-
-			t = time.localtime()
-			current_time = time.strftime("%H:%M:%S", t)
-
 			print("No new ad found... ", current_time)
 		else:
-			print("New: ", current_ad[0], "Time: ", current_time)
+			await send(ad, current_time)
 
 async def main():
 	coroutines = [search_url(URL_1), search_url(URL_2)]
 	await asyncio.gather(*coroutines)
 
-asyncio.run(main())
+if __name__ == "__main__":
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	init_server()
+
+	asyncio.run(main())
